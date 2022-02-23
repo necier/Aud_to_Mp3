7dcdbf743ce62df43534b1c5f2c40d88ed774e58
necier <37684178+necier@users.noreply.github.com>
2022-02-23 17:26:53 +0800

    Update test.bat

--- AUD2MP3v1.03/src/test.bat
+++ AUD2MP3v1.03/src/test.bat
@@ -1,11 +1,13 @@
 @echo off
 format %1
 chcp 65001
+cd ..
 set home=%cd%
-md tmpForPCM
 cd src
+set into=%cd%
+md tmpForPCM
 for /r %1 %%i in (*.aud) do (
-silk_v3_decoder.exe %%i "%home%\tmpForPCM\%%~ni.pcm"
+silk_v3_decoder.exe %%i "%into%\tmpForPCM\%%~ni.pcm"
 ffmpeg -y -f s16le -ar 24000 -threads 6 -ac 1 -loglevel quiet -i %%i "%home%\mp3\%%~ni.mp3"
 )
 rd /s /q "%home%\tmpForPCM"
