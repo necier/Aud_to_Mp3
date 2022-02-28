04c27b9c3a8b7215d9738d28ed5c19e9ddb77a35
necier <37684178+necier@users.noreply.github.com>
2022-02-28 22:02:08 +0800

    Update test.bat

--- AUD2MP3v1.03/src/test.bat
+++ AUD2MP3v1.03/src/test.bat
@@ -8,7 +8,7 @@ set into=%cd%
 md tmpForPCM
 for /r %1 %%i in (*.aud) do (
 silk_v3_decoder.exe %%i "%into%\tmpForPCM\%%~ni.pcm"
-ffmpeg -y -f s16le -ar 24000 -threads 6 -ac 1 -loglevel quiet -i %%i "%home%\mp3\%%~ni.mp3"
+ffmpeg -y -f s16le -ar 24000 -threads 6 -ac 1 -loglevel quiet -i "%into%\tmpForPCM\%%~ni.pcm" "%home%\mp3\%%~ni.mp3"
 )
 rd /s /q "%into%\tmpForPCM"
 
